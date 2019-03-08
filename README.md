d3a248e7138e9a95fce7c1b36a59acea6e6d4b6e
黑岛 <heidao@qunhemail.com>
2019-03-08 15:52:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## How to use Painter in taro
+
+如何在taro框架中使用Painter
---
+++ src/pages/index/index.js
@@ -0,0 +1,50 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button } from '@tarojs/components'
+import Card from '../../palette/card'
+import './index.css'
+
+let imagePath = ''
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页',
+    usingComponents: {
+      'painter': '/components/Painter/components/painter/painter'
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      template: {},
+    }
+  }
+
+  componentDidMount () {
+    this.setState({
+      template: new Card().palette()
+    })
+  }
+
+  onImgOK(e) {
+    imagePath = e.detail.path;
+    console.log(e);
+  }
+
+  saveImage() {
+    Taro.saveImageToPhotosAlbum({
+      filePath: imagePath,
+    });
+  }
+
+  render () {
+    return (
+      <View className='index'>
+        <painter customStyle='margin-left:40rpx' palette={this.state.template} onImgOK={this.onImgOK} />
+        <Button className='save-button' onClick={this.saveImage}>保存</Button>
+      </View>
+    )
+  }
+}
+
